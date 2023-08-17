43dedd82f0ea92c05ac1e1bf36372f568c837c9d
RosalesJhordi <1428060@senati.pe>
2023-08-17 15:18:51 -0500

    Prueba uno

--- Op2/src/main/java/com/mycompany/op2/Op2.java
+++ Op2/src/main/java/com/mycompany/op2/Op2.java
@@ -4,13 +4,25 @@
 
 package com.mycompany.op2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alumno
  */
 public class Op2 {
 
+    //comenzando por segunda vez
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Creacion de Variables
+        
+        int n1,n2,suma,resta,mult,div;
+        String op;
+        
+        // importamos la clase scanner
+        Scanner a1 = new Scanner(System.in);
+        // Priemra visualizacion
+        System.out.print("INGRESE SU OPCION \n 1: SUMA \n 2: RESTA \n 3: MULTIPLICACION \n 3: DIVISION \n Opcion: ");
+        op = a1.next();
     }
 }
